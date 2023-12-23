26099fd95dbb77f86645c7db2b2d7179a6c2c7c2
shanurrahman <shanur.cse.nitap@gmail.com>
2023-12-23 21:18:06 +0530

    🎉 init(fix exports): incorrect export
    There were some exports that were incorrectly placed in another barrel files

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,21 @@
-export * from './models'
-export * from './api'
+import { CopilotApi, SwaggerApi, ChatApi, FlowApi, ActionApi } from "./api";
+
+
+
+class OpenCopilotConnect {
+  public copilot: CopilotApi;
+  public swagger: SwaggerApi;
+  public chat: ChatApi;
+  public flow: FlowApi;
+  public action: ActionApi;
+
+  constructor(baseUrl: string) {
+    this.copilot = new CopilotApi(baseUrl);
+    this.swagger = new SwaggerApi(baseUrl);
+    this.chat = new ChatApi(baseUrl);
+    this.flow = new FlowApi(baseUrl);
+    this.action = new ActionApi(baseUrl);
+  }
+}
+
+export default OpenCopilotConnect;
