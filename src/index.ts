b0011ee1c53ca26643d5d0be0d2fb59866277958
shanurrahman <shanur.cse.nitap@gmail.com>
2023-12-24 00:36:43 +0530

    Removing swagger apis

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,15 @@
-import { CopilotApi, SwaggerApi, ChatApi, FlowApi, ActionApi } from "./api";
+import { CopilotApi, ChatApi, FlowApi, ActionApi } from "./api";
 
 
 
 class OpenCopilotConnect {
   public copilot: CopilotApi;
-  public swagger: SwaggerApi;
   public chat: ChatApi;
   public flow: FlowApi;
   public action: ActionApi;
 
   constructor(baseUrl: string) {
     this.copilot = new CopilotApi(baseUrl);
-    this.swagger = new SwaggerApi(baseUrl);
     this.chat = new ChatApi(baseUrl);
     this.flow = new FlowApi(baseUrl);
     this.action = new ActionApi(baseUrl);
